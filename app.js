3510d8f1ac7607f2a45c8e2cd975cf5e769422f1
LordHarsh <56753150+LordHarsh@users.noreply.github.com>
2023-03-28 16:03:34 +0530

    updated

--- app.js
+++ app.js
@@ -1,9 +1,60 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = require('./models/user');
 
-const app = express()
+mongoose.connect('mongodb://localhost:27017/myapp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
 
-app.get('/', (req, res)=>{
-    res.send('Hello World')
-})
+const app = express();
+app.use(express.json());
 
-app.listen(5000)
+// Route for serving the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+
+// Signup route
+app.post('/signup', (req, res) => {
+  const { email, password } = req.body;
+  const user = new User({ email, password });
+  user.save((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error signing up');
+    } else {
+      res.status(200).send('Signup successful');
+    }
+  });
+});
+
+// Login route
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error logging in');
+    } else if (!user) {
+      res.status(401).send('User not found');
+    } else {
+      user.comparePassword(password, (err, isMatch) => {
+        if (isMatch && !err) {
+          const token = jwt.sign({ email }, 'SECRET_KEY', { expiresIn: '1h' });
+          res.status(200).json({ token });
+        } else {
+          res.status(401).send('Incorrect password');
+        }
+      });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+// Hash the password before saving the user
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Compare passwords for login
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
